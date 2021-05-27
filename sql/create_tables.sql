618a532252cbd6b80dc0c5bb39ea0a4beabb19d9
nhrt <hartingerniklas@gmail.com>
2021-05-27 08:42:47 +0200

    Updates table

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,12 +1,26 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+create table projects (
+    project_id uuid primary key
+);
+
+create table issues (
+    issue_id uuid primary key
+);
+
 create table users (
-    id numeric primary key,
+    id uuid primary key DEFAULT uuid_generate_v4(),
     username varchar(50),
     name varchar(50),
     last_name varchar(50),
     date_of_birth date,
     create_time timestamp,
     global_role varchar(20),
-    last_login timestamp null
+    last_login timestamp null,
 );
 
-insert into users values (0, 'xXRausAusDenSchulden69Xx', 'Peter', 'Zwegat', current_date, current_timestamp, 'SCHULDENBERATER', current_timestamp)
+
+insert into issues values ('a3974d24-5735-410c-b109-ad262755d4d3');
+insert into projects values ('54ed2c8e-054d-4fb0-81ac-d7ed726b1879');
+insert into users values ('a443ffd0-f7a8-44f6-8ad3-87acd1e91042', 'Peter_Zwegat', 'Peter', 'Zwegat', current_date, current_timestamp, 'normal', current_timestamp)
+
