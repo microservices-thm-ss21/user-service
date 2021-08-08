066c3a43cbb1f05dbda9cae5a5bea0d738846a7a
Niels Korschinsky <niels.korschinsky@mni.thm.de>
2021-08-08 14:27:28 +0200

    Update Readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# user-service
+
+Please refer to the [README](https://git.thm.de/microservicesss21/orga/-/blob/master/README.md) of the Orga-Repository for more information.
+
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -15,5 +15,5 @@ create table users (
 
 -- the following users all have the password "password" hashed to "{bcrypt}$2a$10$t.3KubtyXsOEO9.eoWRzTOZMMTuiPvLrhSe9YFzgzV2J8kVtIOiLa"
 insert into users values ('a443ffd0-f7a8-44f6-8ad3-87acd1e91042', 'Peter_Zwegat', '{bcrypt}$2a$10$t.3KubtyXsOEO9.eoWRzTOZMMTuiPvLrhSe9YFzgzV2J8kVtIOiLa', 'Peter', 'Zwegat', 'peter.zwegat@mni.thm.de', current_date, current_timestamp, 'ADMIN', current_timestamp);
-insert into users values ('a443ffd0-f7a8-44f6-8ad3-87acd1e91043', 'Kim-Jong-Dos', '{bcrypt}$2a$10$t.3KubtyXsOEO9.eoWRzTOZMMTuiPvLrhSe9YFzgzV2J8kVtIOiLa', 'Kim', 'Jong-Dos', 'kim@nord-korea.com', current_date, current_timestamp, 'USER', current_timestamp);
+insert into users values ('a443ffd0-f7a8-44f6-8ad3-87acd1e91043', 'Kim-Jong-Dos', '{bcrypt}$2a$10$t.3KubtyXsOEO9.eoWRzTOZMMTuiPvLrhSe9YFzgzV2J8kVtIOiLa', 'Kim', 'Jong-Dos', 'kim@nord-korea.com', current_date, current_timestamp, 'SUPPORT', current_timestamp);
 insert into users values ('a443ffd0-f7a8-44f6-8ad3-87acd1e91044', 'Kim-Jong-On', '{bcrypt}$2a$10$t.3KubtyXsOEO9.eoWRzTOZMMTuiPvLrhSe9YFzgzV2J8kVtIOiLa', 'Kim', 'Jong-On', 'lil-kim@nord-korea.com', current_date, current_timestamp, 'USER', current_timestamp)
